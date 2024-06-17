881bf26a291460e172ab8c3aba1e47aa79fec477
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-06-17 18:08:30 +0530

    Create Header.tsx

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white text-center">
+      <h1>SkillTrack</h1>
+    </header>
+  );
+};
+
+export default Header;
