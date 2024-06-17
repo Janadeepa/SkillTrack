609de9a97e1101f617cb99c6e0ce7ec98e5154d2
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-06-17 18:06:55 +0530

    Create auth.js

---
+++ backend/src/utils/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).send('Access denied. No token provided.');
+
+  try {
+    const decoded = jwt.verify(token, config.jwtSecret);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).send('Invalid token.');
+  }
+};
+
+module.exports = authMiddleware;
